4c217cff8f33b3b306dbb795f022f3c7e431a643
Olena Boldysheva <adv55577@gmail.com>
2022-04-11 16:22:58 +0300

    config storybook, add simple component & tests

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,6 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  // stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.js"],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
